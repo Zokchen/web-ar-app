719d212ff7b41994690af3b01d320c8530e661b9
Zokchen <101956909+Zokchen@users.noreply.github.com>
2024-06-29 14:41:47 +0545

    update in scripts

--- src/ARScene.js
+++ src/ARScene.js
@@ -1,25 +1,7 @@
 // src/ARScene.js
-import React, { useEffect } from "react";
+import React from "react";
 
 const ARScene = () => {
-  useEffect(() => {
-    // Ensure aframe and aframe-ar scripts are loaded
-    const scriptAFrame = document.createElement("script");
-    scriptAFrame.src = "https://aframe.io/releases/1.2.0/aframe.min.js";
-    scriptAFrame.async = true;
-    document.body.appendChild(scriptAFrame);
-
-    const scriptARjs = document.createElement("script");
-    scriptARjs.src =
-      "https://cdn.rawgit.com/jeromeetienne/ar.js/1.6.2/aframe/build/aframe-ar.js";
-    scriptARjs.async = true;
-    document.body.appendChild(scriptARjs);
-
-    return () => {
-      document.body.removeChild(scriptAFrame);
-      document.body.removeChild(scriptARjs);
-    };
-  }, []);
 
   return (
     <a-scene embedded arjs="sourceType: webcam;">
